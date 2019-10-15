26c120acc011f1bf629e8295d77903f4b62acce2
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2019-10-15 14:31:19 +0100

    add tokenizer

--- src/tokenize.js
+++ src/tokenize.js
@@ -6,6 +6,75 @@ const {
   isQuote,
 } = require('./identify');
 
-const tokenize = () => {};
+const tokenize = (input) => {
+  let cursor = 0;
+  const tokens = [];
+
+  while (cursor < input.length) {
+    const character = input[cursor];
+
+    if (isParenthesis(character)) {
+      tokens.push({
+        type: 'Parenthesis',
+        value: character,
+      });
+      cursor++;
+      continue;
+    }
+
+    if (isWhitespace(character)) {
+      cursor++;
+      continue;
+    }
+
+    if (isNumber(character)) {
+      let number = character;
+
+      while(isNumber(input[++cursor])) {
+        number += input[cursor];
+      }
+
+      tokens.push({
+        type: 'Number',
+        value: Number(number),
+      });
+      continue;
+    }
+
+    if (isLetter(character)) {
+      let letter = character;
+
+      while(isLetter(input[++cursor])) {
+        letter += input[cursor];
+      }
+
+      tokens.push({
+        type: 'Name',
+        value: letter,
+      });
+      continue;
+    }
+
+    if (isQuote(character)) {
+      let string = '';
+
+      while(!isQuote(input[++cursor])) {
+        string += input[cursor];
+      }
+
+      tokens.push({
+        type: 'String',
+        value: string,
+      });
+
+      cursor++;
+      continue;
+    }
+
+    throw new Error(`${character} is not valid.`);
+  }
+
+  return tokens;
+};
 
 module.exports = { tokenize };
